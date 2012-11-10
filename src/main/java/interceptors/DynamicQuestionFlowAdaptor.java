739d41493f6af52f8d948e185b3c1d3f2ba94bfb
tim carter <twrcarter@hotmail.co.uk>
2012-11-10 16:06:41 +0000

    add interceptor to try and help dyno qs

---
+++ src/main/java/interceptors/DynamicQuestionFlowAdaptor.java
@@ -0,0 +1,35 @@
+package interceptors;
+
+import org.springframework.webflow.definition.StateDefinition;
+import org.springframework.webflow.execution.EnterStateVetoException;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.FlowExecutionListenerAdapter;
+import org.springframework.webflow.execution.RequestContext;
+import org.springframework.webflow.execution.View;
+
+public class DynamicQuestionFlowAdaptor extends FlowExecutionListenerAdapter {
+	
+	@Override
+	public void eventSignaled(RequestContext context, Event event) {
+		// TODO Auto-generated method stub
+		super.eventSignaled(context, event);
+		System.out.println("***eventSignaled:");
+	}
+	
+	@Override
+	public void stateEntering(RequestContext context, StateDefinition state)
+			throws EnterStateVetoException {
+		// TODO Auto-generated method stub
+		super.stateEntering(context, state);
+		System.out.println("***stateEntering:");
+	}
+	
+	@Override
+	public void viewRendering(RequestContext context, View view,
+			StateDefinition viewState) {
+		// TODO Auto-generated method stub
+		super.viewRendering(context, view, viewState);
+		System.out.println("***viewRendering:");
+	}
+
+}
